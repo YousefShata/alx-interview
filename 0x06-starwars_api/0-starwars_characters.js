6107fccf782474a4fe453164e7764506010cd522
YousefShata <yossef_shata_a@yahoo.com>
2024-08-11 11:52:02 -0700

    promise added

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,6 +4,23 @@ const request = require('request');
 const url = 'https://swapi.dev/api/films/';
 const movieId = process.argv[2];
 
+const fetchCharacter = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        return reject(error);
+      }
+
+      try {
+        const character = JSON.parse(body);
+        resolve(character.name);
+      } catch (Error) {
+        return console.error('Failed to parse movie data:', Error);
+      }
+    });
+  });
+};
+
 request(`${url}${movieId}/`, (error, response, body) => {
   if (error) {
     return console.error('Error fetching movie data:', error);
@@ -14,15 +31,8 @@ request(`${url}${movieId}/`, (error, response, body) => {
 
   const movie = JSON.parse(body);
   const characterUrls = movie.characters;
-
-  characterUrls.forEach(url => {
-    request(url, (charError, charResponse, charBody) => {
-      if (charError) {
-        return console.error('Error fetching character data:', charError);
-      }
-
-      const character = JSON.parse(charBody);
-      console.log(character.name);
+  Promise.all(characterUrls.map(fetchCharacter))
+    .then(characters => {
+      characters.forEach(character => console.log(character));
     });
-  });
 });
